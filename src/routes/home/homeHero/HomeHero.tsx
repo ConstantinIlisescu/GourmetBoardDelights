c2e2530ca9b5f22be15e0812a6cb29163bffdae5
Costa Ilisescu <coste_ilisescu@yahoo.com>
2024-08-04 09:06:59 +0100

    chnage main page text colour

--- src/routes/home/homeHero/HomeHero.tsx
+++ src/routes/home/homeHero/HomeHero.tsx
@@ -5,11 +5,11 @@ const HomeHero = () => {
     <section className="relative h-[80vh] bg-cover bg-center home-hero-image">
       <div className="absolute inset-0 hero-overlay"></div>
 
-      <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white px-4">
-        <h1 className="text-6xl md:text-8xl great-vibes-regular mb-4">
+      <div className="relative z-10 flex flex-col items-center justify-center h-full text-center px-4">
+        <h1 className="text-6xl md:text-8xl great-vibes-regular mb-4 text-white">
           Sweets that make you <br /> Feel Happy
         </h1>
-        <p className="text-lg md:text-xl mb-8 montserrat-regular">
+        <p className="text-lg md:text-xl mb-8 montserrat-regular text-white">
           With Our Hand Crafted Chocolate
         </p>
         <Button href="/products">PRODUCTS</Button>
